a2b68ea19b85cd86f1fe81f33563bd09e5a2e5df
yaojiach <jiachen.yao@outlook.com>
2019-03-25 10:45:45 -0700

    Modify starter.py

--- airflow/dags/starter.py
+++ airflow/dags/starter.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2015, 6, 1),
+    'start_date': datetime(2018, 6, 1),
     'email': ['airflow@airflow.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -15,7 +15,7 @@ default_args = {
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
+    'end_date': datetime(2018, 6, 8),
 }
 
 dag = DAG('starter', default_args=default_args, schedule_interval=timedelta(1))
