eebbf6f0714ed5f43d5a6da68d0644b853dc7e1e
yaojiach <jiachen.yao@outlook.com>
2019-03-26 15:18:01 -0700

    Add email notification util

--- airflow/dags/starter.py
+++ airflow/dags/starter.py
@@ -3,18 +3,104 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
 
+# Utility function
+sender = 'me@amazon.com'
+receiver = 'you@amazon.com'
+server = 'smtp.amazon.com'
+password = 'password'
+
+def send_mail(
+    send_from, 
+    send_to,
+    subject, 
+    message, 
+    cc=None,
+    server="localhost", 
+    use_tls=True,
+    port=587,
+    username='',
+    password=''
+    ):
+    """Compose and send email with provided info and attachments.
+
+    Arguments:
+        send_from (str): from name
+        send_to (str): to name
+        subject (str): message title
+        message (str): message body
+        files (list[str]): list of file paths to be attached to email
+        cc (str): cc name
+        server (str): mail server host name
+        use_tls (bool): use TLS mode
+        port (int): port number
+        username (str): server auth username
+        password (str): server auth password
+    """
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.base import MIMEBase
+    from email.mime.text import MIMEText
+    from email.utils import formatdate
+    from email import encoders
+
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = send_to
+    if cc:
+        msg['Cc'] = cc
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message))
+
+    smtp = smtplib.SMTP(server, port)
+    if use_tls:
+        smtp.ehlo()
+        smtp.starttls()
+    smtp.ehlo()
+    smtp.login(username, password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.quit()
+
+def failed_mail_callback(ctx):
+    title = 'Airflow alert: {task} failed.'.format(**ctx)
+    body = """
+    Airflow alert: {task} failed
+    """.format(**ctx)
+    send_mail(
+        sender,
+        receiver,
+        title,
+        body,
+        server=server,
+        username=sender,
+        password=password
+    )
+
+def retry_mail_callback(ctx):
+    title = 'Airflow alert: {task} retrying.'.format(**ctx)
+    body = """
+    Airflow alert: {task} retrying
+    """.format(**ctx)
+    send_mail(
+        sender,
+        receiver,
+        title,
+        body,
+        server=server,
+        username=sender,
+        password=password
+    )
+
+
+# Define DAG
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2018, 6, 1),
-    'email': ['airflow@airflow.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
     'end_date': datetime(2018, 6, 8),
 }
 
