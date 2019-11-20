993ea5d7493a9b2554bb32ed5aa6f7c7a0932c22
JC <jiachen.yao@outlook.com>
2019-11-20 14:47:24 -0500

    Previous auth script failed unexpectedly

--- airflow/scripts/auth.py
+++ airflow/scripts/auth.py
@@ -1,14 +1,31 @@
+# import airflow
+# from airflow import models, settings
+# from airflow.contrib.auth.backends.password_auth import PasswordUser
+
+
+# user = PasswordUser(models.User())
+# user.username = 'admin'
+# user.email = 'admin@mail.box'
+# user.password = 'admin'
+
+# session = settings.Session()
+# session.add(user)
+# session.commit()
+# session.close()
+
+# alternative
 import airflow
 from airflow import models, settings
 from airflow.contrib.auth.backends.password_auth import PasswordUser
-
-
 user = PasswordUser(models.User())
-user.username = 'admin'
-user.email = 'admin@mail.box'
-user.password = 'admin'
+user.username = 'user'
+user.email = 'user@email.com'
+user.password = 'password'
 
-session = settings.Session()
+from sqlalchemy import create_engine
+engine = create_engine("postgresql://airflow:airflow@postgres:5432/airflow")
+
+session = settings.Session(bind=engine)
 session.add(user)
 session.commit()
 session.close()
